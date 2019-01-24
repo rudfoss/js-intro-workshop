5f26b4bcfe54de029d5609aa4f7b663ada11cfda
Thomas <thomas.rudfoss@gmail.com>
2019-01-24 13:43:51 +0100

    Added basic tests

--- client/src/models/Item.test.js
+++ client/src/models/Item.test.js
@@ -1,7 +1,14 @@
-import {Item} from "./item"
+import { Item } from "./Item"
 
 describe("Item", () => {
 	it("should be defined", () => {
 		expect(Item).toBeDefined()
+	})
+
+	it("should set default values for properties", () => {
+		const inst = new Item()
+		expect(inst.text).toEqual("")
+		expect(inst.done).toEqual(false)
+		expect(inst.doneTime).not.toBeDefined()
 	})
 })
